05a0fbd443fccdc7cb3cf0f4f8474a4704903825
Ashwani Kumar Dwivedi(UST <291504@ust.com>
2025-06-26 17:18:11 +0530

    learn till section 8

---
+++ LearnSpringFramework02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -0,0 +1,27 @@
+package com.in28minutes.learn_spring_framework.examples.a1;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class DependencyInjectionLauncherApplication {
+
+
+    public static void main(String[] args) {
+
+        try(var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)){
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+        }
+
+
+    }
+}
--- LearnSpringFramework02/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ LearnSpringFramework02/src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,7 @@ public class GameRunner {
 //    MarioGame game;
     private GamingConsole game;
 
-    public GameRunner(GamingConsole game) {
+    public GameRunner(@Qualifier("superContraGame") GamingConsole game) {
         this.game = game;
     }
 
